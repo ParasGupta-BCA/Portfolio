31993d2c17370714802f9160eeebb066039a4926
Paras Gupta <parasgupta4494@gmail.com>
2025-12-13 22:30:23 +0530

    done

---
+++ portfolio-react/src/app/page.tsx
@@ -0,0 +1,9 @@
+import TimelineDemo from "@/components/timeline-demo";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-background">
+      <TimelineDemo />
+    </main>
+  );
+}
